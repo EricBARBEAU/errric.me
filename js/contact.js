391a6fc49eb342daee17cbb331d582802730cbac
Eric BARBEAU <eric@roomorama.com>
2016-11-30 17:08:54 +0800

    Layout improvements, bridge section, email subject...

--- js/contact.js
+++ js/contact.js
@@ -7,12 +7,12 @@ $(document).ready(function()
 		$(".pt-page").css("transform","scale(0.94, 0.9)");
 		$(".contactOverlay").css("visibility","visible");
 		$(".contactOverlay").animate({opacity: "0.6"},300);
-		$("#Contact").animate({top: "20%"},500);
+		$("#Contact").addClass("visible",500);
 	});
 
 	$(".close-btn").click(function()
 	{
-		$("#Contact").animate({top: "-1200px"},500);
+		$("#Contact").removeClass("visible",500);
 		$(".contactOverlay").css("visibility","hidden");
 		$(".contactOverlay").animate({opacity: "0"},300);
 		$(".pt-page").css("transform","scale(1, 1)");

387480ff57ac76d556c35ea1858d08367a37b519
Eric Barbeau <ebarbeau.graphics@gmail.com>
2021-05-05 21:58:00 +0800

    working dark/light theme switcher

--- js/switch.js
+++ js/switch.js
@@ -4,20 +4,27 @@ $(document).ready(function()
 	{
 	 if(($(this).attr("data-goto"))==-1)
 	 {
-	   $(".logo").hide('slide',500,'ease-in-out');
-	   $(".nav-btn").css("color","#f8f6f5");
+		$(".logo").hide('slide',300,'ease-in-out');
+
+		if (document.body.classList.contains('theme-light')) {
+			$(".nav-btn").css("color","#f8f6f5");
+		}
 
 	   setTimeout(function() {
         $('.project-visual').addClass('active');
         $(".project-content").addClass('drop');
-    }, 500);
+    }, 300);
 	 }
 	 else
 	 {
+	 	$(".logo").show('slide',300,'ease-in-out'); 
+
 	 	$(".project-content").removeClass('drop');
 	 	$('.project-visual').removeClass('active');
-	 	$(".nav-btn").css("color","#262427");
-	  $(".logo").show('slide',500,'ease-in-out'); 
+
+	 	if (document.body.classList.contains('theme-light')) {
+			$(".nav-btn").css("color","#262427");
+		}
 	 }
 	});
 });		
--- js/theme-switch.js
+++ js/theme-switch.js
@@ -5,9 +5,11 @@ $(document).ready(function()
 		if (document.body.classList.contains('theme-light')) {
 			$(document.body).removeClass('theme-light');
 			$(document.body).addClass('theme-dark');
-		} else {
+			$(".nav-btn").css("color","#f8f6f5");
+		} else if (document.body.classList.contains('theme-dark')) {
 			$(document.body).removeClass('theme-dark');
 			$(document.body).addClass('theme-light');
+			$(".nav-btn").css("color","#262427");
 		}
 	});
 });	

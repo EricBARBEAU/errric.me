e1cbfc8d9f850b2cfa91e6abe81a2bc110eeb61d
Eric Barbeau <ebarbeau.graphics@gmail.com>
2022-09-23 15:17:51 +0200

    Contact page update

--- js/_src/main.js
+++ js/_src/main.js
@@ -1,24 +1,24 @@
 $(document).ready(function() 
 {
 
-	// Contact function
-	$(".nav-btn-2").click(function()
-	{
-		$("#TopBar").addClass('shrink');
-		$(".pt-page").css("transform","scale(0.94, 0.9)");
-		$(".contactOverlay").css("visibility","visible");
-		$(".contactOverlay").animate({opacity: "0.6"},300);
-		$("#Contact").addClass("visible",500);
-	});
+	// // Contact function
+	// $(".nav-btn-2").click(function()
+	// {
+	// 	$("#TopBar").addClass('shrink');
+	// 	$(".pt-page").css("transform","scale(0.94, 0.9)");
+	// 	$(".contactOverlay").css("visibility","visible");
+	// 	$(".contactOverlay").animate({opacity: "0.6"},300);
+	// 	$("#Contact").addClass("visible",500);
+	// });
 
-	$(".close-btn").click(function()
-	{
-		$("#Contact").removeClass("visible",500);
-		$(".contactOverlay").css("visibility","hidden");
-		$(".contactOverlay").animate({opacity: "0"},300);
-		$(".pt-page").css("transform","scale(1, 1)");
-		$("#TopBar").removeClass('shrink');
-	});
+	// $(".close-btn").click(function()
+	// {
+	// 	$("#Contact").removeClass("visible",500);
+	// 	$(".contactOverlay").css("visibility","hidden");
+	// 	$(".contactOverlay").animate({opacity: "0"},300);
+	// 	$(".pt-page").css("transform","scale(1, 1)");
+	// 	$("#TopBar").removeClass('shrink');
+	// });
 
 	// Switch function
 	$(".pt-trigger").click(function()

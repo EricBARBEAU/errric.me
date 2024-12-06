e792b1bb1901e753b8c6613996456941973448d7
EricBARBEAU <ebarbeau.graphics@gmail.com>
2024-12-06 13:13:20 +0100

    trying to copy the gh-pages repo here

---
+++ src/modules/header.js
@@ -0,0 +1,14 @@
+
+function Nav() {
+  return (
+    <nav id="TopBar">
+      <a href="#" className="logo" alt="Home"></a>
+      <ul class="SubNav">
+        <li><a href="https://errric.darkroom.com/" className="nav-btn nav-btn-1" target="blank">Photos</a></li>
+        <li><a className="nav-btn nav-btn-2" >About me</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;

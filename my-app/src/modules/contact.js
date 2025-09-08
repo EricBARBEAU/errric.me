6898a306507f293e6e1753b5b0b193bb03461719
EricBARBEAU <ebarbeau.graphics@gmail.com>
2025-09-08 11:23:36 +0200

    Update on pdf url for resume, switching off from gh-pages branch to use proper flow

--- my-app/src/modules/contact.js
+++ my-app/src/modules/contact.js
@@ -32,7 +32,7 @@ function Contact() {
             </div>
             <div className="ctas">
               <a href="mailto:hello@errric.me?subject=Hey Eric, let's work together!" className="contact-btn contact-btn-1">write me</a>
-              <a href="https://cdn.jsdelivr.net/gh/EricBARBEAU/errric.me@master/EricBARBEAU_Resume2024.pdf" target="_blank" rel="noreferrer" className="contact-btn contact-btn-2">my resume</a>
+              <a href="https://cdn.jsdelivr.net/gh/EricBARBEAU/errric.me@master/EricBARBEAU_Resume2025.pdf" target="_blank" rel="noreferrer" className="contact-btn contact-btn-2">my resume</a>
             </div>
           </div>
           <img src={eric_new} className="about_photo" alt="EricBarbeau" />

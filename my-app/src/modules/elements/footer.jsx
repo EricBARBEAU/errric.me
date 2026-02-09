8939f476864212e536522306e3b2d79b93df88f1
EricBARBEAU <ebarbeau.graphics@gmail.com>
2026-02-09 12:07:49 +0100

    footer styling

--- my-app/src/modules/elements/footer.jsx
+++ my-app/src/modules/elements/footer.jsx
@@ -1,5 +1,9 @@
 // Assets
 const logo_white = '/img/errric-logo_white.svg';
+const linkedin = '/img/logo-linkedin.svg';
+const github = '/img/logo-github.svg';
+const dribbble = '/img/logo-dribbble.svg';
+const producthunt = '/img/logo-producthunt.svg';
 
 function Footer() {
 
@@ -14,17 +18,21 @@ function Footer() {
     	</div>
     	<div className="links">
     		<p>Find me on</p>
-    		<a className="footer_link footer_link-linkedin" href="" >
-    			
+    		<a className="footer_link footer_link-linkedin" href="https://www.linkedin.com/in/eric-barbeau/" >
+    			<img src={linkedin} alt="linkedin" />
+          LINKEDIN
     		</a>
-    		<a className="footer_link footer_link-github" href="" >
-    			
+    		<a className="footer_link footer_link-github" href="https://github.com/EricBARBEAU" >
+    			<img src={github} alt="linkedin" />
+          GITHUB
     		</a>
-    		<a className="footer_link footer_link-dribbble" href="" >
-    			
+    		<a className="footer_link footer_link-dribbble" href="https://dribbble.com/EB_Graphics" >
+    			<img src={dribbble} alt="linkedin" />
+          DRIBBBLE
     		</a>
-    		<a className="footer_link footer_link-producthunt" href="" >
-    			
+    		<a className="footer_link footer_link-producthunt" href="https://www.producthunt.com/@errric" >
+    			<img src={producthunt} alt="linkedin" />
+          PRODUCT HUNT
     		</a>
     	</div>
     </div>
